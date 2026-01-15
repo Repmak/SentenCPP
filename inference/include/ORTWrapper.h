622874b9f5db19d020d3799a52b089915b186ecd
Justin Kamper <jkamper@fulcrumgt.com>
2026-01-15 00:00:02 +0000

    embedding model seems to work?

--- inference/include/ORTWrapper.h
+++ inference/include/ORTWrapper.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <EncoderInterface.h>
 #include <onnxruntime_cxx_api.h>
 #include <vector>
 
@@ -9,19 +10,18 @@ namespace nlp::inference {
         public:
             ORTWrapper(const std::string& model_path);
 
-            std::vector<float> run(
-                const std::vector<int64_t>& input_ids,
-                const std::vector<int64_t>& mask,
-                const std::vector<int64_t>& type_ids
-            );
+            std::vector<float> run(const std::vector<encoder::Token>& tokens);
 
         private:
             Ort::Env env;
             Ort::Session session;
             Ort::MemoryInfo memory_info;
 
-            std::vector<const char*> input_names = {"input_ids", "attention_mask", "token_type_ids"};
-            std::vector<const char*> output_names = {"output"};
+            std::vector<std::string> input_names;
+            std::vector<std::string> output_names;
+
+            static std::vector<float> perform_pooling(const std::vector<float>& raw_logits, size_t num_tokens);
+            static std::vector<float> calculate_softmax(const std::vector<float>& logits);
     };
 
 } // namespace nlp::inference
